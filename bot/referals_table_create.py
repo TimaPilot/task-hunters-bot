6b341763e69e3a888122759a3a2b26c944490ecf
Tima Pilot <dreamer.of.sleeps@gmail.com>
2025-04-18 01:23:10 +0300

    Reboot

--- bot/referals_table_create.py
+++ bot/referals_table_create.py
@@ -1,7 +1,23 @@
-CREATE TABLE referrals (
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+dsn = os.getenv("DATABASE_URL")
+
+conn = psycopg2.connect(dsn)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS referrals (
     id SERIAL PRIMARY KEY,
     inviter_id BIGINT NOT NULL,
     invited_id BIGINT NOT NULL UNIQUE,
     confirmed BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+)
+""")
+
+conn.commit()
+cursor.close()
+conn.close()
